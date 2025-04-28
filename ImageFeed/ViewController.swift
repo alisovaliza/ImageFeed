7b4cca0aba8d0f31ffa748c016c50ef652aeb2f5
alissliz <alissliz@yandex.ru>
2025-04-28 21:38:53 +0300

    First Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Lisa Alisova on 25.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
